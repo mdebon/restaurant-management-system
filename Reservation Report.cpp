087aa41fe8a0d2727e032d40bd74525fa00fb106
thaivinhla99 <104872789+thaivinhla99@users.noreply.github.com>
2022-05-05 02:04:12 -0500

    Add files via upload

---
+++ Reservation Report.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include "reservation mngnt.cpp"
+
+using namespace std;
+
+//function to show Account details
+void reservationReport() {
+
+
+    string name;
+    bool reservationStatus;
+    string reservationTime;
+    string reservationDate;
+
+
+
+    cout << "-------------------------------------------------------------------------" << endl;
+
+    cout << "NAME OF RESERVER : " << name << endl;
+    cout << "DATE OF RESERVATION    : " << reservationDate << endl;
+    cout << "TIME OF RESERVATION  : " << reservationTime << endl;
+    cout << "reservationStatus  :" << reservationStatus << endl;
+    
+    cout << "-------------------------------------------------------------------------" << endl;
+
+    system("pause");
+
+}
