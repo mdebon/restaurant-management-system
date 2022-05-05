19c34e69b4454fbf45024f608e0baa97eec0e261
Omar G <104709115+OmarGzz713@users.noreply.github.com>
2022-05-05 09:37:19 -0500

    Add files via upload

--- MenuandMenuUpdating.cpp
+++ MenuandMenuUpdating.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
-#include <fstream>
-
-
 
 using namespace std;
 
 int main()
-{   
-    //define array of strings
-    string itemName[11] = { "1", "2", "3","4", "5", "6","7", "8","9", "10" };
-    //get array size
-    int arraySize = *(&itemName + 1) - itemName;
-    //exception handling
-    try { 
-        //file is opened to update menu 
-        ofstream file("menuUpdateMgmt.txt", std::ofstream::out);
+{
+    int order = 1;
+
+    int itemName1 = 1, itemName2 = 2, itemName3 = 3, itemName4 = 4, itemName5 = 5; //item menu slots
+    const float itemPrice1 = 6.95, itemPrice2 = 5.75, itemPrice3 = 7.25, itemPrice4 = 8.95, itemPrice5 = 4.95; //prices for each item
+    bool availability1 = 1, availability2 = 1, availability3 = 1, availability4 = 1, availability5 = 1; //if availability is 1, then it is available. 
   
-        if (file.is_open())
-        {
-            //will store contents into file from array 
-            for (int i = 1; i < arraySize; i++)
-            {
-                file << itemName[i] << "\n";
-            }
-            file.close(); 
-        }
-        else cout << "Error with file! Please try again";
-    }
-    catch (const char* msg) {
-        cerr << msg << endl;
-    }
+    cout << setw(25) << "***** Menu *****\n"
+        << itemName1 << " Pho " << setw(17) << "$" << itemPrice1 << setw(5) << availability1 << endl
+        << itemName2 << " Chalupa " << setw(13) << "$" << itemPrice2 << setw(5) << availability2 << endl
+        << itemName3 << " Croissant " << setw(11) << "$" << itemPrice3 << setw(5) << availability3 << endl
+        << itemName4 << " Krabby Patty " << setw(8) << "$" << itemPrice4 << setw(5) << availability4 << endl
+        << itemName5 << " Monkey D. Luffy " << setw(5) << "$" << itemPrice5 << setw(5) << availability5 << endl;
 
-    std::ifstream file("menuUpdateMgmt.txt");
-
-    if (file.is_open()) //Opens file to edit
-        cout << "\n\t ** Menu ** \t";
-        cout << "\n";
-        std::cout << file.rdbuf(); 
-
-
-
-        
+    
+    system("PAUSE");
+    return 0;
 }
