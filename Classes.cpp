19596796ecd356c769777cdcf59581315e6d7a5b
mdebon <104861310+mdebon@users.noreply.github.com>
2022-05-05 20:22:18 -0500

    Update Classes.cpp

--- Classes.cpp
+++ Classes.cpp
@@ -15,30 +15,30 @@ class MenuUpdateReport
 		{
 
 
-        string itemName;
-        bool availability;
-        float price;
-        string menuUpdateDate;
-        string menuUpdateTime;
-        string priceUpdateDate;
-        string priceUpdateTime;
+        	string itemName;
+        	bool availability;
+        	float price;
+        	string menuUpdateDate;
+        	string menuUpdateTime;
+        	string priceUpdateDate;
+        	string priceUpdateTime;
 
 
 
-        cout << "-------------------------------------------------------------------------" << endl;
+        	cout << "-------------------------------------------------------------------------" << endl;
 
-        cout << "ITEM NAME : " << itemName << endl;
-        cout << "Price     : " << price << endl;
-        cout << "Avalability  : " << availability << endl;
-        cout << "MENU UPDATE DATE  :" << menuUpdateDate << endl;
-        cout << "MENU UPDATE TIME  :" << menuUpdateTime << endl;
-        cout << "PRICE UPDATE DATE  :" << priceUpdateDate << endl;
-        cout << "PRICE UPDATE TIME  :" << priceUpdateTime << endl;
+        	cout << "ITEM NAME : " << itemName << endl;
+        	cout << "Price     : " << price << endl;
+        	cout << "Avalability  : " << availability << endl;
+        	cout << "MENU UPDATE DATE  :" << menuUpdateDate << endl;
+        	cout << "MENU UPDATE TIME  :" << menuUpdateTime << endl;
+        	cout << "PRICE UPDATE DATE  :" << priceUpdateDate << endl;
+        	cout << "PRICE UPDATE TIME  :" << priceUpdateTime << endl;
 
     
-        cout << "-------------------------------------------------------------------------" << endl;
+        	cout << "-------------------------------------------------------------------------" << endl;
 
-  	}
+  		}
 };
 
 class reservationReport
